fec3fcca2fab4261d43aee51dadf8bea49c1ff43
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-02-13 14:13:38 +0000

    working example

--- app.js
+++ app.js
@@ -35,79 +35,20 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-//db
-//    .sequelize
-//    .sync({ force: true })
-//    .complete(function(err) {
-//        if (err) {
-//            throw err
-//        } else {
-//            http.createServer(app).listen(app.get('port'), function(){
-//                console.log('Express server listening on port ' + app.get('port'))
-//            })
-//        }
-//    });
-
-//var joe = db['User'].build({name: 'Joe'});
-//var jeb = db['User'].build({name: 'Jeb'});
-//var abe = db['User'].build({name: 'Abe'});
-//_.each([joe,jeb,abe], function(i){ i.save(); });
-//joe.setFriends([jeb, abe]).success(function() {
-//  console.log('set');
-//})
-//joe.save();
-
 var fs        = require('fs')
   , Sequelize = require('sequelize')
   , sequelize = new Sequelize('sequelize', 'root', 'asdasd', {logging:console.log})
 
-var Series, Trainer, Video
- 
-// Series has a trainer_id=Trainer.id foreign reference key after we call Trainer.hasMany(series)
-Series = sequelize.define('Series', {
-    title:        Sequelize.STRING,
-    sub_title:    Sequelize.STRING,
-    description:  Sequelize.TEXT,
+var User = sequelize.define('User', {
+    username: Sequelize.STRING,
+    birthday: Sequelize.DATE
+})
 
-    // Set FK relationship (hasMany) with `Trainer`
-    trainer_id: {
-        type: Sequelize.INTEGER,
-        references: "Trainer",
-        referencesKey: "id"
-        }
-    },{
-   tableName: 'Series'
-});
-
-Trainer = sequelize.define('Trainer', {
-    first_name: Sequelize.STRING,
-    last_name:  Sequelize.STRING
-});
-
-// Video has a series_id=Series.id foreign reference key after we call Series.hasOne(Video)...
-Video = sequelize.define('Video', {
-    title:        Sequelize.STRING,
-    sequence:     Sequelize.INTEGER,
-    description:  Sequelize.TEXT,
-
-    // set relationship (hasOne) with `Series`
-    series_id: {
-        type: Sequelize.INTEGER,
-        references: "Series",
-        referencesKey: "id"
-    }
-});
-
-//Trainer.hasMany(Series);
-//Series.hasOne(Video);
-Trainer.hasMany(Series);
-Series.hasOne(Video);
-
-//_.each([,jeb,abe], function(i){ i.save(); });
-sequelize.sync({force:true});
-//var s1 = Series.build({title: 'S1'});
-//var t1 = Trainer.build({first_name: 'Jon T'});
-//s1.save();
-//t1.save();
-//sequelize.sync();
-//s1.setTrainer(t1);
+sequelize.sync().success(function() {
+    User.create({
+        username: 'sdepold',
+        birthday: new Date(1986, 06, 28)
+    }).success(function(sdepold) {
+        console.log(sdepold.values)
+    })
+})
